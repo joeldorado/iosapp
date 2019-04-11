b6aa682ee2a55f0bda3809aa56f7bedf6a0b6d94
doradoj <doradoaguilusjoel@gmail.com>
2019-04-10 18:24:42 -0700

    ios app v2 log in menus

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,35 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
+import {NavController} from '@ionic/angular';
+import {Tab2Page} from '../tab2/tab2.page';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+  constructor(public navCtrl: NavController) {
+  }
+
+
+   usuario="asened";
+   pass="asened";
+   Fusuario="";
+   Fpass="";
+   messages="Bienvenido";
+    todo = {}
+    logForm() {
+     if(this.Fusuario===this.usuario && this.Fpass === this.pass){
+      this.messages="Entraste";
+      console.log("Log in ok send to another tab");
+      this.navCtrl.navigateForward("../tab2/tab2");  
+      }else{
+       this.messages="Usuario o contraseña incorrectos";
+        console.log("Usuario o contraseña incorrectos");
+      }  
+    
+    }
+   
+
+    
+
+}
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,32 @@
 import { Component } from '@angular/core';
 
+import { MenuController } from '@ionic/angular';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+//  <link href=""/>
+export class Tab2Page {
+  constructor(public menu: MenuController) {
+
+  }
+  openFirst() {
+    this.menu.enable(true, 'first');
+    this.menu.open('first');
+  }
+
+  openEnd() {
+    this.menu.open('end');
+  }
+
+  openCustom() {
+    this.menu.enable(true, 'custom');
+    this.menu.open('custom');
+  }
+
+
+
+
+
+}
